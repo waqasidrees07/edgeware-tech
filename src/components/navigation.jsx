45f02f2e23d6d0a0376b7c3529b7a3ebcf1505c9
waqasidrees07 <waqasidrees15@gmail.com>
2025-02-11 02:11:27 +0500

    updates

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Edgeware Technologies
+            <img src="img/edgeware-logo/transparent-pngs/edgeware-logo-01.png" alt="logo" />
           </a>{" "}
         </div>
 
